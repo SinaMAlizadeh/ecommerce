fa949fd85fc0011daaf17a86121c18eaa99ddb93
SinaMAlizadeh <sina.alizadeh.120@gmail.com>
2020-03-30 21:57:51 +0430

    add style to menu item

--- src/components/manu-item/menu-item.component.jsx
+++ src/components/manu-item/menu-item.component.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './menu-item.styles.scss';
 const MenuItem = ({title , imageUrl ,size}) => {
     return ( 
-        <div
-        style={{
-            backgroundImage:`url(${imageUrl})`
-        }}
+        <div     
         className={`${size} menu-item`}>
-        <div className="contetn">
+
+<div className='background-image'
+  style={{
+    backgroundImage:`url(${imageUrl})`
+}}
+>
+</div>
+        <div className="content">
             <h1 className='title'>
-                {title}
+                {title.toUpperCase()}
             </h1>
             <span className='subtitle'>
                 SHOP NOW
